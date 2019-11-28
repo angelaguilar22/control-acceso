0e211efe9fad605698b03ddd672a18f7eeb1fd85
Angel.Aguilar <angel.martinez@acrux.mx>
2019-11-28 01:18:14 -0600

    change theme

--- src/app/pages/codigos-qr/codigos-qr.component.ts
+++ src/app/pages/codigos-qr/codigos-qr.component.ts
@@ -6,8 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./codigos-qr.component.scss']
 })
 export class CodigosQrComponent implements OnInit {
+  public myAngularxQrCode: string = 'Algo';
 
-  constructor() { }
+  ngxQrcode2 = 'https://www.npmjs.com/package/ngx-qrcode2';
+  techiediaries = 'https://www.npmjs.com/~techiediaries';
+  letsboot = 'https://www.letsboot.com/';
+  
+  constructor() {
+    this.myAngularxQrCode = 'Your QR code data string';
+  }
 
   ngOnInit() {
   }
